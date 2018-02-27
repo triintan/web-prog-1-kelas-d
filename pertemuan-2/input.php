e405815a6b1dc0ed97e62a37c9b2f1cccba06d69
triintan <triintan33@gmail.com>
2018-02-27 16:55:46 +0700

    pertemuan 2

---
+++ pertemuan-2/input.php
@@ -0,0 +1,6 @@
+<form action="proses.php"method="post">
+    Nama Anda<input type ="text"name="nama"><br>
+    Email <input type="text"name="email"><br>
+    <input type="submit" value="proses">
+
+    </form>
---
+++ pertemuan-2/proses.php
@@ -0,0 +1,7 @@
+<h2> 
+Selamat datang , <?php echo $_POST["nama"]; ?>
+</h2>
+<br>
+<hr>
+Email anda [<?php echo $_POST["email"]; ?>]
+telah terekam
